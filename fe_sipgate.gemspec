b0fe613965ead55e5765c2c5d7ad7ab6528b2604
James Robertson <digital.robertson@gmail.com>
2022-08-04 14:33:06 +0100

    bug fix: Method *balance* is now working.

--- fe_sipgate.gemspec
+++ fe_sipgate.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'fe_sipgate'
-  s.version = '0.3.0'
+  s.version = '0.4.0'
   s.summary = 'Uses Ferrum to login to Sipgate with your credentials to ' + 
       'query the account balance.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/fe_sipgate.rb']
-  s.add_runtime_dependency('ferrumwizard', '~> 0.2', '>=0.2.1')
+  s.add_runtime_dependency('ferrumwizard', '~> 0.3', '>=0.3.4')
   s.signing_key = '../privatekeys/fe_sipgate.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/fe_sipgate.rb
+++ lib/fe_sipgate.rb
@@ -5,23 +5,38 @@
 require 'ferrumwizard'
 
 
+class FeSipgateErr < Exception
+end
+
 class FeSipgate
 
   def initialize(debug: false, headless: true, cookies: nil)
 
     @fw = FerrumWizard.new('https://login.sipgate.com/', headless: headless,
                            cookies: cookies, debug: debug)
+    @fw.scan_page if cookies
+
   end
 
-  def login(usernamex=nil, passwordx=nil, username: usernamx, password: passwordx)
+  def login(usernamex=nil, passwordx=nil, username: usernamex, password: passwordx)
     @fw.login(username, password)
   end
 
+  def save_cookies(file)
+    @fw.save_cookies file
+  end
+
   def balance()
-    @fw.account.balance
+
+   raise FeSipgateErr, 'not logged in?' unless @fw.browser.title == 'sipgate | App'
+
+    r = @fw.links.keys.find {|x| x =~ /Balance:/}
+    balance = r[/(?<=Balance: ).*/]
+
   end
 
   def quit()
     @fw.quit
   end
+
 end
